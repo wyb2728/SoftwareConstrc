c27cbb2bf1cdb33592acc62f4b8f2d3bb55046b1
wyb2728 <152183508+wyb2728@users.noreply.github.com>
2024-03-17 23:08:08 +0800

    Add files via upload

---
+++ Assignment1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            double a=0,b=0;
+            s=Console.ReadLine();
+            a=Double.Parse(s);
+            s = Console.ReadLine();
+            b = Double.Parse(s);
+            s = Console.ReadLine();
+            switch (s)
+            {
+                case "+":
+                    a=a+b;
+                    break;
+                case "-":
+                    a=a-b;
+                    break;
+                case "*":
+                    a=a*b;
+                    break;
+                case "/":
+                    a=a/b;
+                    break;
+                default:
+                    Console.WriteLine("Illegal input");
+                    break;
+            }
+
+            Console.WriteLine($"{a}");
+        }
+    }
+}
---
+++ Assignment1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int a=int.Parse(s);
+            s = textBox3.Text;
+            int b=int.Parse(s);
+            s = textBox2.Text;
+            int c=0;
+            switch (s)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Illegal input");
+                    break;
+            }
+            textBox4.Text = c.ToString();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
